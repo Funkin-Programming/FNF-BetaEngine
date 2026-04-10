c7f75863a396774fe7c31fdcd5e8723f15e7f5c0
LumiCoder <100051827+JoseXD2@users.noreply.github.com>
2026-04-10 16:09:39 -0300

    a

--- source/Main.hx
+++ source/Main.hx
@@ -181,7 +181,7 @@ class Main extends Sprite
 			}
 		}
 
-		errMsg += "\nUncaught Error: " + e.error + "\nPlease report this error to the GitHub page: https://github.com/ShadowMario/FNF-PsychEngine\n\n> Crash Handler written by: sqirra-rng";
+		errMsg += "\nUncaught Error: " + e.error + "\nPlease report this error to the GitHub page: https://github.com/DeveloperPorting/Psych-Engine-0.7.3-Mobile\n\n> Crash Handler written by: sqirra-rng";
 
 		/*if (!FileSystem.exists("crash/"))
 			FileSystem.createDirectory("crash/");
